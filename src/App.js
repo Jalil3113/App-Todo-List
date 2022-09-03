a3c346c35a30f5dd4d8968cad2ba20ad36504c40
Jalil3113 <110029763+Jalil3113@users.noreply.github.com>
2022-09-04 02:01:29 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const App = () => {
+	return (
+		<div>
+			<h1>To-Do List</h1>
+      <ul>git
+	Tambah Kegiatan 
+</ul>
+<form action="#">
+	<input type="text" name="name" placeholder="Tuliskan sesuatu..." />
+	<input type="submit" value="Tambah" />
+</form>
+		</div>
+	)
+}
+
+export default App
